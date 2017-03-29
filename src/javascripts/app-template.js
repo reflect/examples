0746989fdedcbea57420dd5cdec8b60e7dd0d199
Sebastian <sprokuski@gmail.com>
2017-03-29 13:46:50 -0700

    Update github fork and theme links, remove github attribute from json

--- src/javascripts/app-template.js
+++ src/javascripts/app-template.js
@@ -63,7 +63,7 @@ $(function(){
     var $header = $('#app-header');
 
     $header.append(`
-      <a class="fork-me desktop-only" href="` + app.github + `">
+      <a class="fork-me desktop-only" href="https://github.com/reflect/examples/blob/master/apps/` + app.name + `">
         <img src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png">
       </a>
       <div class="lead">
@@ -77,7 +77,7 @@ $(function(){
         </div>
         <div class="theme">
           <h6>Theme</h6>
-          <a href="#">` + app.theme + `</a>
+          <a href="https://github.com/reflect/examples/blob/master/apps/themes/` + app.theme + `.scss">` + app.theme + `</a>
         </div>
         <div class="dataset">
           <h6>Dataset</h6>
