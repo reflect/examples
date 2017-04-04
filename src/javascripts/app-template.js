2ac8b6a84825210512f2e61da34351a7aee891d5
Sebastian <sprokuski@gmail.com>
2017-04-04 16:32:27 -0700

    Minor tweaks including button styles, main summary copy, and hr fade-out

--- src/javascripts/app-template.js
+++ src/javascripts/app-template.js
@@ -40,7 +40,7 @@ $(function(){
     (app.features).forEach(function(feature) {
       var featureId = feature.split(' ').join('_');
       var link = featureUrls[featureId];
-      features.push('<a class="feature ' + featureId + '" href="' + link + '">' + feature + '</a>');
+      features.push('<a target="_blank" class="feature ' + featureId + '" href="' + link + '">' + feature + '</a>');
     });
 
     features = features.sort().join('');
@@ -67,7 +67,7 @@ $(function(){
     var $header = $('#app-header');
 
     $header.append(`
-      <a class="fork-me desktop-only" href="https://github.com/reflect/examples/blob/master/apps/` + app.category + `/` + app.name + `.html">
+      <a target="_blank" class="fork-me desktop-only" href="https://github.com/reflect/examples/blob/master/apps/` + app.category + `/` + app.name + `.html">
         <img src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png">
       </a>
       <div class="lead">
