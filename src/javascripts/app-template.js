4cd8d2bb6ca8ffaf89552b0fd4a3e5f2faf136e4
Sebastian <sprokuski@gmail.com>
2017-04-05 14:34:11 -0700

    Change styling of fork me ribbon

--- src/javascripts/app-template.js
+++ src/javascripts/app-template.js
@@ -69,9 +69,7 @@ $(function(){
     var $header = $('#app-header');
 
     $header.append(`
-      <a target="_blank" class="fork-me desktop-only" href="https://github.com/reflect/examples/blob/master/apps/` + app.category + `/` + app.name + `.html">
-        <img src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png">
-      </a>
+      <a target="_blank" class="fork-me desktop-only" href="https://github.com/reflect/examples/blob/master/apps/` + app.category + `/` + app.name + `.html">View on Github</a>
       <div class="lead">
         <h1 class="title">` + title + `</h1>
         <h2>` + app.description + `</h2>
